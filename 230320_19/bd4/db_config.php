829d75fb9f5004c7ab9108bf511c8c07a6f8a53f
yedeon <59893384+yedeon@users.noreply.github.com>
2020-03-30 18:04:21 -0600

    Delete db_config.php

--- 230320_19/bd4/db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_USER', "thyeaexcxloshs"); // Usuario
-define('DB_PASSWORD', "6dafe7c8d26ab92c5b248e66321985114198fce9cd75eb90b799e33765b16a05"); // Password
-define('DB_DATABASE', "d1o711frv23ueg"); // Nombre de la base de datos
-define('DB_SERVER', "ec2-35-174-88-65.compute-1.amazonaws.com"); //host or server
-?>
